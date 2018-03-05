4331a90d31849af5c97c97cbe78f82ec2e0485fc
ronaldjk <ronaldjiwonkim@gmail.com>
2018-03-05 16:11:14 -0500

    Another edit

--- src/com/gc/controller/HomeController.java
+++ src/com/gc/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-/*
+/**
  * Project Name: Modern Ghost
  * @author Emily Blanford, Nick Soule, Jordan Zwart, Ronald Kim
  *
