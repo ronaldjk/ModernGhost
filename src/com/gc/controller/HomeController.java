6a3d9fd35b15543554de7c7bcb2a5fffc579c44b
nicksoule <nicksoule@live.com>
2018-03-06 14:43:55 -0500

    connected to database

--- src/com/gc/controller/HomeController.java
+++ src/com/gc/controller/HomeController.java
@@ -1,23 +1,19 @@
 package com.gc.controller;
 
-import java.io.IOException;
+import java.util.ArrayList;
 
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-import org.json.JSONArray;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gc.model.Address;
+import com.gc.util.HibernateUtil;
 
-import com.gc.util.APICredentials;
-import com.gc.util.Address;
 
 
 /**
@@ -29,63 +25,79 @@ import com.gc.util.Address;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping("welcome")
 	public ModelAndView findGhost(@RequestParam("address") String address) {
-		String test = Address.formatAddress(address);
-		System.out.println(test);
-		Double lat = Address.getLat(test);
-		Double lng = Address.getLng(test);
-		
-		
-		System.out.println("lat: "+ lat + ", lng: "+ lng);
-		
+		System.out.println("yo");
 		String text = "";
-		String text2 = "";
-		String text3 = "";
-		try {
-			HttpClient http14 = HttpClientBuilder.create().build();
-			HttpHost host14 = new HttpHost("data.detroitmi.gov", 443, "https");
-			HttpGet getPage14 = new HttpGet("/resource/hhs5-b2n3.json?$$app_token=" + APICredentials.DATADETROIT_KEY);
-			HttpResponse resp14 = http14.execute(host14, getPage14);
-			String jsonString14 = EntityUtils.toString(resp14.getEntity());
-			JSONArray arr14 = new JSONArray(jsonString14);
+		
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 
-			for (int i = 0; i < 3; i++) {
-
-				text += arr14.getJSONObject(i).get("x") + " " + arr14.getJSONObject(i).get("y");
-				System.out.println(text);
-			}
-			
-			HttpClient http15 = HttpClientBuilder.create().build();
-			HttpHost host15 = new HttpHost("data.detroitmi.gov", 443, "https");
-			HttpGet getPage15 = new HttpGet("/resource/sr29-szd3.json?$$app_token=" + APICredentials.DATADETROIT_KEY);
-			HttpResponse resp15 = http15.execute(host15, getPage15);
-			String jsonString15 = EntityUtils.toString(resp15.getEntity());
-			JSONArray arr15 = new JSONArray(jsonString15);
-
-			for (int i = 0; i < 3; i++) {
-
-				text2 += arr15.getJSONObject(i).get("x") + " " + arr15.getJSONObject(i).get("y");
-				System.out.println(text2);
-			}
-			
-			HttpClient http16 = HttpClientBuilder.create().build();
-			HttpHost host16 = new HttpHost("data.detroitmi.gov", 443, "https");
-			HttpGet getPage16 = new HttpGet("/resource/g2xp-q8wj.json?$$app_token=" + APICredentials.DATADETROIT_KEY);
-			HttpResponse resp16 = http16.execute(host16, getPage16);
-			String jsonString16 = EntityUtils.toString(resp16.getEntity());
-			JSONArray arr16 = new JSONArray(jsonString16);
-
-			for (int i = 0; i < 3; i++) {
-
-				text3 += arr16.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(0) + " " + arr16.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(1);
-				System.out.println(text3);
-			}
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+        Session session = sessionFactory.openSession();
+        // the transaction represents the unit of work/actual implementation of code
+        Transaction tx = session.beginTransaction();
+        Criteria crit = session.createCriteria(Address.class);
+        ArrayList<Address> ghostList = (ArrayList<Address>) crit.list();
+        text = ghostList.get(1).getAddress();
+        
+        
+        
+        tx.commit();
+        session.close();
+//		String test = Address.formatAddress(address);
+//		System.out.println(test);
+//		Double lat = Address.getLat(test);
+//		Double lng = Address.getLng(test);
+//		
+//		
+//		System.out.println("lat: "+ lat + ", lng: "+ lng);
+//		
+//		String text = "";
+//		String text2 = "";
+//		String text3 = "";
+//		try {
+//			HttpClient http14 = HttpClientBuilder.create().build();
+//			HttpHost host14 = new HttpHost("data.detroitmi.gov", 443, "https");
+//			HttpGet getPage14 = new HttpGet("/resource/hhs5-b2n3.json?$$app_token=" + APICredentials.DATADETROIT_KEY);
+//			HttpResponse resp14 = http14.execute(host14, getPage14);
+//			String jsonString14 = EntityUtils.toString(resp14.getEntity());
+//			JSONArray arr14 = new JSONArray(jsonString14);
+//
+//			for (int i = 0; i < 3; i++) {
+//
+//				text += arr14.getJSONObject(i).get("x") + " " + arr14.getJSONObject(i).get("y");
+//				System.out.println(text);
+//			}
+//			
+//			HttpClient http15 = HttpClientBuilder.create().build();
+//			HttpHost host15 = new HttpHost("data.detroitmi.gov", 443, "https");
+//			HttpGet getPage15 = new HttpGet("/resource/sr29-szd3.json?$$app_token=" + APICredentials.DATADETROIT_KEY);
+//			HttpResponse resp15 = http15.execute(host15, getPage15);
+//			String jsonString15 = EntityUtils.toString(resp15.getEntity());
+//			JSONArray arr15 = new JSONArray(jsonString15);
+//
+//			for (int i = 0; i < 3; i++) {
+//
+//				text2 += arr15.getJSONObject(i).get("x") + " " + arr15.getJSONObject(i).get("y");
+//				System.out.println(text2);
+//			}
+//			
+//			HttpClient http16 = HttpClientBuilder.create().build();
+//			HttpHost host16 = new HttpHost("data.detroitmi.gov", 443, "https");
+//			HttpGet getPage16 = new HttpGet("/resource/g2xp-q8wj.json?$$app_token=" + APICredentials.DATADETROIT_KEY);
+//			HttpResponse resp16 = http16.execute(host16, getPage16);
+//			String jsonString16 = EntityUtils.toString(resp16.getEntity());
+//			JSONArray arr16 = new JSONArray(jsonString16);
+//
+//			for (int i = 0; i < 3; i++) {
+//
+//				text3 += arr16.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(0) + " " + arr16.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(1);
+//				System.out.println(text3);
+//			}
+//		} catch (ClientProtocolException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		return new ModelAndView("welcome", "message", text);
 	}
 }
