feead611fe6f69c7c1d430384fbebfb8c8056d70
nicksoule <nicksoule@live.com>
2018-03-15 10:51:09 -0400

    fixed api calculations for 2016 api

--- src/com/gc/calc/Calculations.java
+++ src/com/gc/calc/Calculations.java
@@ -36,8 +36,10 @@ public class Calculations {
 		String gLng = "";
 		for (int i = 0; i < arr.length(); i++) {
 			if (year == 16 || year == 2016) {
-				ghostLat = (double) arr.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(0);
-				ghostLng = (double) arr.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(1);
+				gLat = arr.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(0).toString();
+				gLng = arr.getJSONObject(i).getJSONObject("location").getJSONArray("coordinates").get(1).toString();
+				ghostLat = Double.parseDouble(gLat);
+				ghostLng = Double.parseDouble(gLng);
 			} else {
 				gLat = arr.getJSONObject(i).get("y").toString();
 				gLng = arr.getJSONObject(i).get("x").toString();
